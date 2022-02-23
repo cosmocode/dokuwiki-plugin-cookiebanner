62aa4e54778ec4533cb1005343140c2947af0221
Krabyyx <ahsystems@outlook.fr>
2022-02-23 15:15:35 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Krabyyx <ahsystems@outlook.fr>
+ */
+$lang['js']['yay']             = 'Accepter';
+$lang['js']['nay']             = 'Refuser';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Krabyyx <ahsystems@outlook.fr>
+ */
+$lang['script']                = 'Le code de script à exécuter une fois que l\'utilisateur a donné son consentement.	';
